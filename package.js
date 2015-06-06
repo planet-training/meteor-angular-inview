a1657c6fe770fc9a3747e3ab790b8fbb7f78858c
Max Bruchmann <bruchmann@signal7.de>
2015-06-06 19:00:20 +0200

    added describe part

--- package.js
+++ package.js
@@ -1,14 +1,13 @@
 Package.describe({
   name: 'planettraining:meteor-angular-inview',
   version: '1.5.0',
-  // Brief, one-line summary of the package.
-  summary: '',
+  
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',
-  describe : 'angular inview meteor package'
+  summary : 'angular inview meteor package'
 });
 
 Package.onUse(function(api) {

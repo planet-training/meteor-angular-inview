2cc179efd47fb0dc1cdb6447e59e423a65e029f1
Max Bruchmann <bruchmann@signal7.de>
2015-06-06 19:06:20 +0200

    added readme content

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 Package.describe({
   name: 'planettraining:meteor-angular-inview',
   version: '1.5.0',
-  
+
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/planet-training/meteor-angular-inview.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',

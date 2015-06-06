82e91de7c966251bb9200e2d642ea55b37da0f17
Max Bruchmann <bruchmann@signal7.de>
2015-06-06 18:57:00 +0200

    added describe part

--- package.js
+++ package.js
@@ -7,7 +7,8 @@ Package.describe({
   git: '',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: 'README.md',
+  describe : 'angular inview meteor package'
 });
 
 Package.onUse(function(api) {
